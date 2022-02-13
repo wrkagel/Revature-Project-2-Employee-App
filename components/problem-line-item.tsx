81afe023a77408cc2590322212d0616b0b74df20
Walter <wrkagel@uw.edu>
2022-02-12 19:32:31 -0800

    Working image resizing

--- components/problem-line-item.tsx
+++ components/problem-line-item.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Dimensions, Image, Pressable, StyleSheet, Text, View, Button } from "react-native";
 import Problem from "../models/problem";
 import ProblemRoutes from "../routes/problem-routes";
 
@@ -7,8 +7,28 @@ import ProblemRoutes from "../routes/problem-routes";
 
 
 export default function ProblemLineItem(props:{item:Problem,problems:Problem[],index:number,setProblems:Function}){
-    const[reviewed, setReviewed]= useState<{}>()
-    
+    const[reviewed, setReviewed]= useState<{}>();
+    const [expandedImage, setExpandedImage] = useState<boolean>(false);
+    const [image, setImage] = useState<{uri:string, height:number, width:number, shrunkHeight:number, shrunkWidth:number} | null>(null);
+
+    useEffect(()=> {
+        if(props.item.photoLink) {
+            Image.getSize(props.item.photoLink, (width, height) => {
+                let shrunkHeight = height;
+                let shrunkWidth = width;
+                const {width:screenWidth, height:screenHeight} = Dimensions.get('screen');
+                while(height > screenHeight || width > screenWidth) {
+                    height *= 0.95;
+                    width *= 0.95;
+                }
+                while(shrunkHeight > screenHeight * 0.25 || shrunkWidth > screenWidth * 0.25) {
+                    shrunkHeight *= 0.95;
+                    shrunkWidth *= 0.95;
+                }
+                setImage({uri:props.item.photoLink, height, width, shrunkHeight, shrunkWidth});
+            });
+      }
+    },[]);
 
     useEffect(()=>{
         if(!reviewed) return;
@@ -29,7 +49,23 @@ export default function ProblemLineItem(props:{item:Problem,problems:Problem[],i
             <Text style={styles.bodyText}><Text style={styles.boldText}>Report Date: </Text>{new Date(props.item.submittedTime).toLocaleString()}</Text>
             <Text style={styles.bodyText}><Text style={styles.boldText}>Details: </Text>{props.item.desc}</Text>
             <Text style={[styles.bodyText, styles.boldText]}>Status: {props.item.status}</Text>
-            {Boolean(props.item.photoLink) && <Image source={{uri: props.item.photoLink}} style={styles.image}/>}
+            {image && (props.item.status !== "reviewed" || expandedImage ? <Pressable onPress={() => {
+                setExpandedImage(!expandedImage);
+            }}>
+            <Image source={{ uri: image.uri}} 
+            style={
+                {width: (expandedImage ? image.width : image.shrunkWidth), 
+                height: (expandedImage ? image.height : image.shrunkHeight),
+                alignSelf:"center",
+                marginVertical:10}} />
+            </Pressable>
+            :
+            <Pressable style={styles.button} onPress={() => {
+                setExpandedImage(!expandedImage);
+            }}>
+                <Text style={styles.buttonText}>Show Image</Text>
+            </Pressable>
+            )}
             {props.item.status !== "reviewed" ? <Pressable style={styles.button} onPress={()=>setReviewed({...reviewed})}  >
                 <Text style={styles.buttonText}>Mark as reviewed</Text>
             </Pressable> 
@@ -64,10 +100,5 @@ const styles = StyleSheet.create({
     buttonText:{
         textAlign:"center",
         color:"white"
-    },
-    image:{
-        width:200,
-        height:200,
-        marginHorizontal:10
     }
 });
